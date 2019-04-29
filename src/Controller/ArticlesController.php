1ce812a2c1491394609e5396633bc863f1b5164a
Josh <joshforcier@gmail.com>
2019-04-29 18:38:18 -0500

    hw10

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,32 @@
+<?php
+// src/Controller/ArticlesController.php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+class ArticlesController extends AppController
+{
+
+    public function initialize()
+    {
+        parent::initialize();
+
+        $this->loadComponent('Flash'); // Include the FlashComponent
+        $this->loadComponent('Paginator'); // Include the FlashComponent
+
+        $this->Auth->allow(['tags']);
+    }
+
+    public function index()
+    {
+        $articles = $this->Paginator->paginate($this->Articles->find());
+        $this->set(compact('articles'));
+    }
+
+    public function view($slug)
+    {
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        $this->set(compact('article'));
+    }
+}
